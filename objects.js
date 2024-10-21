4bd3e9543b5430c567d8caa2a5b3c14d5ba2a9b5
nedarctic <nedarctic@gmail.com>
2024-10-21 22:19:49 +0300

    create object method example

--- objects.js
+++ objects.js
@@ -1,14 +1,4 @@
-const phone = {
-    brand: "Vivo",
-    model: "Vivo X200 Pro",
-    year: "2024",
-    storage: "1TB",
-    ram: "16GB",
-    price: "$1000"
-}
+const proto = {greet() {return "Hello!";}};
 
-Object.seal(phone); // prevents adding new properties but allows modifying existing ones
-
-phone.camera = "Zeiss"; // adding new property
-phone.price = "1300$"; // modifying an existing property
-console.log(Object.entries(phone));
+const obj = Object.create(proto);
+console.log(obj.greet());

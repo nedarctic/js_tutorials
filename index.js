264f56bed1eeb97a264dc461b02fc5422de45261
nedarctic <nedarctic@gmail.com>
2024-10-21 15:53:37 +0300

    keys array method example

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const phones = ['Samsung', 'iPhone', 'Google Pixel', 'Vivo', 'Honor', 'iQoo', 'Huawei', 'Redmi'];
 
-for(let [index, phone] of phones.entries()){
-    console.log(`Index: ${index} Phone: ${phone}`);
+for(key of phones.keys()){
+    console.log(key);
 }

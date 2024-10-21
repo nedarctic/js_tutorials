46df7103d137dbdab76796dee24ac1fe2c5d1eaa
nedarctic <nedarctic@gmail.com>
2024-10-21 15:23:50 +0300

    reverse array method example

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 const phones = ['Samsung', 'iPhone', 'Google Pixel', 'Vivo', 'Honor', 'iQoo', 'Huawei', 'Redmi'];
 
-const isPhonesSomeIncludeO = phones.some(phone => phone.includes('o'));
-console.log(isPhonesSomeIncludeO);
+console.log(phones.reverse());

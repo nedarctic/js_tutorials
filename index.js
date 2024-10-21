58fa6da3407dba2048e159f8714beae7415a9a0e
nedarctic <nedarctic@gmail.com>
2024-10-21 15:14:07 +0300

    forEach array method example

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 const phones = ['Samsung', 'iPhone', 'Google Pixel', 'Vivo', 'Honor', 'Huawei'];
 
-const phonesSeparatedByCommas = phones.reduce((previous, current,  currentId) => {
-    let sentence = previous + ", " + current;
-    if(currentId == phones.length - 1){
-        sentence += ".";
-    }
-    return sentence;
-});
-console.log(phonesSeparatedByCommas);
+phones.forEach(phone => console.log(phone));

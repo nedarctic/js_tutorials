ff012da8a25d9cc37fdc2b0d4d57da45d21c63eb
nedarctic <nedarctic@gmail.com>
2024-10-21 21:52:32 +0300

    keys object method example

---
+++ objects.js
@@ -0,0 +1,11 @@
+const phone = {
+    model: "Samsung S24 Ultra",
+    battery: "5000mAh",
+    display: "LTPO",
+    price: "1000$",
+    year: 2024,
+    storage: "1TB",
+    RAM: "16GB"
+}
+
+console.log(Object.keys(phone));

21039e984256c819b63beb4d3a5c09493374c824
nedarctic <nedarctic@gmail.com>
2024-10-21 14:47:59 +0300

    shift array method example

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const phones = ['Samsung', 'iPhone', 'Google Pixel', 'Vivo', 'Honor', 'Huawei'];
 
-console.log('Phones array length before pushing', phones.length);
-phones.push('iQoo');
-console.log('Phones array length after pushing:', phones.length);
+const shiftedItem = phones.shift();
+console.log(shiftedItem);

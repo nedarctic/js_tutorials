dacf2d9a743827bd22be2ae5117cd57ee216a049
nedarctic <nedarctic@gmail.com>
2024-10-21 14:51:37 +0300

    slice array method example

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const phones = ['Samsung', 'iPhone', 'Google Pixel', 'Vivo', 'Honor', 'Huawei'];
-console.log('Length of phones before unshifting:', phones.length);
-const newLength = phones.unshift('iQoo');
-console.log('Length of phones arr after unshifting:', newLength);
+
+const slicedArr = phones.slice(1, 3);
+console.log(slicedArr);

3ee2819ebee3cfae9f494159bcb72df25f393e56
nedarctic <nedarctic@gmail.com>
2024-10-21 22:30:57 +0300

    is object method example

--- objects.js
+++ objects.js
@@ -1,4 +1,13 @@
-const proto = {greet() {return "Hello";}};
+const phone = {
+    brand: "Samsung",
+    model: "Samsung S24 Ultra"
+}
 
-const obj = Object.create(proto);
-console.log(Object.getPrototypeOf(obj));
+const samsung = {
+    brand: "Samsung",
+    model: "Samsung S24 Ultra"
+}
+
+console.log(Object.is(phone, samsung));
+console.log(Object.is(25, 25));
+console.log(Object.is(NaN, NaN));
